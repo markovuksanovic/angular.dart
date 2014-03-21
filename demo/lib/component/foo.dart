0ab8bcd23f5ac12639e521b5c2b0f50fbe0255b5
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-03-21 14:31:14 +1100

    chore(NgDirective): Example using new module parameter

--- demo/lib/component/foo.dart
+++ demo/lib/component/foo.dart
@@ -4,13 +4,36 @@ import 'package:angular/angular.dart';
 
 @NgComponent(selector: 'foo',
     publishAs: 'ctrl',
-    templateUrl: 'packages/demo/component/foo.html')
+    templateUrl: 'packages/demo/component/foo.html',
+    module: CustomModule)
 class FooComponent {
+  final SomeType someType;
+  final SomeType2 someType2;
 
+  FooComponent(this.someType, this.someType2) {
+    print(someType.i);
+    print(someType2.i);
+  }
 }
 
 class FooModule extends Module {
   FooModule() {
     type(FooComponent);
+    install(new CustomModule());
   }
+}
+
+class CustomModule extends Module {
+  CustomModule() {
+    type(SomeType);
+    factory(SomeType2, (_) => new SomeType2());
+  }
+}
+
+class SomeType {
+  int i = 1;
+}
+
+class SomeType2 {
+  int i = 2;
 }
