ddde11bf57af965d133c8098143fb2da225aa7c1
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-04-04 14:53:11 +1100

    Modify hello world to be able to test functionality.

--- example/web/hello_world.dart
+++ example/web/hello_world.dart
@@ -1,3 +1,4 @@
+import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
 
@@ -8,8 +9,31 @@ class HelloWorldController {
   String name = "world";
 }
 
+@NgDirective(selector: '[test]', map: const {
+    'baseField' : '@baseField'
+})
+class TestDirectiveBase {
+  var baseField;
+}
+
+@NgDirective(selector: '[test]', map: const {
+  'field' : '@field'
+})
+class TestDirective extends TestDirectiveBase {
+  var field;
+  Element element;
+
+  TestDirective(this.element) {
+    element.onClick.listen((e) {
+      print(this.baseField);
+      print(this.field);
+    });
+  }
+}
+
+
 main() {
   applicationFactory()
-      .addModule(new Module()..type(HelloWorldController))
+      .addModule(new Module()..type(HelloWorldController)..type(TestDirective))
       .run();
 }
