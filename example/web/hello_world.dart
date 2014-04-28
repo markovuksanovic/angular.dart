456852c9fbd0c9b0420ce1de6f176a1918a42583
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-04-28 12:25:28 +1000

    Bug

--- example/web/hello_world.dart
+++ example/web/hello_world.dart
@@ -1,15 +1,43 @@
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
 
-@Controller(
-    selector: '[hello-world-controller]',
-    publishAs: 'ctrl')
-class HelloWorld {
-  String name = "world";
+class Item
+{
+  String name, value;
+
+  Item (this.name, this.value);
 }
 
-main() {
-  applicationFactory()
-      .addModule(new Module()..type(HelloWorld))
-      .run();
+@Controller(
+    selector: '[controller]',
+    publishAs: 'ctrl')
+class Test
+{
+  List<Item> items = [new Item ('Alice', 'one'), new Item ('Bob', 'two')];
+  Item item;
+
+  Scope scope;
+
+  Test(Scope this.scope)
+  {
+    scope.watch('item', (newValue, _) {
+      if (newValue != null) {
+        print (newValue.value);
+      }
+    },
+    context: this);
+    scope.apply();
+  }
+}
+
+class TestModule extends Module
+{
+  TestModule ()
+  {
+    type (Test);
+  }
+}
+
+void main() {
+  applicationFactory().addModule(new TestModule()).run();
 }
